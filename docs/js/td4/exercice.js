51b474653140b65e922023441b01063455af4e73
Mathis Fourey <mfourey@info.local>
2024-09-30 11:56:09 +0200

    debut td5

--- docs/js/td4/exercice.js
+++ docs/js/td4/exercice.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded",function(){
     const liste = document.getElementById("liste");
 
     ajouterBtn.addEventListener("click",
+
         function(){
             const texte = inputElement.value;
             if(texte !== ""){
@@ -35,7 +36,6 @@ document.addEventListener("DOMContentLoaded",function(){
 
 function somme(){
     let nbr1, nbr2, sum;
-    nbr1 = Number(document.getElementById("nbr1").value);
         nbr2 = Number(document.getElementById("nbr2").value);
         sum = nbr1 + nbr2;
         document.getElementById("sum").value = sum;
---
+++ docs/pages/td5/Font-tester.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Affichage dynamique de messages</title>
+    <style>
+        .message {
+            font-size: 12px; /* Taille par défaut */
+        }
+    </style>
+</head>
+<body>
+
+<?php
+// Récupération des paramètres de l'URL
+$message = isset($_GET['message']) ? $_GET['message'] : '';
+$color = isset($_GET['color']) ? $_GET['color'] : 'black';
+$size = isset($_GET['size']) && is_numeric($_GET['size']) ? $_GET['size'] : 12;
+
+// Affichage du message si des paramètres sont fournis
+if (!empty($message)) {
+    echo "<div id='message' style='color: $color; font-size: {$size}px;'>$message</div>";
+}else {
+    echo "<p>Veuillez fournir un message, une couleur et une taille.</p>";
+}
+?>
+
+<a href="?message=Message%20rouge&color=red&size=15">Rouge 15</a>
+<a href="?message=message%20vert&color=green&size=30">Vert 30</a>
+<a href="?message=message%20bleu&color=blue&size=50">Bleu 50</a>
+
+<form method="GET">
+    <input type="text" name="message" placeholder="Votre message">
+    <input type="text" name="color" placeholder="Couleur">
+    <input type="number" name="size" placeholder="Taille">
+    <button type="submit">Envoyer</button>
+</form>
+
+<button id="increment">+</button>
+<button id="decrement">-</button>
+
+</body>
+</html>
