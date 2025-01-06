8642836324e3429bdd6eeaddaa1265bba20fc54e
Kenoren <mfourey@info.local>
2025-01-06 11:28:00 +0100

    finition td9 + amélioration index grace au td9

---
+++ docs/data/data.js
@@ -0,0 +1,70 @@
+const data = [
+    {
+      name: 'TD1',
+      title: 'Architecture web',
+      icon: 'fas fa-database',
+      exercises: [
+        { name: 'Exercice 1', link: './pages/td1/exercice1.html' },
+        { name: 'Exercice 2', link: './pages/td1/exercice2.html' },
+      ]
+    },
+    {
+      name: 'TD2',
+      title: 'HTML Structure',
+      icon: 'fas fa-network-wired',
+      exercises: [
+        { name: 'Protocoles', link: './pages/td2/protocoles.html' },
+        { name: 'Configuration', link: './pages/td2/configuration.html' },
+      ]
+    },
+    {
+      name: 'TD3',
+      title: 'HTML Positionnement',
+      icon: 'fas fa-code',
+      exercises: [
+        { name: 'Variables', link: './pages/td3/variables.html' },
+        { name: 'Fonctions', link: './pages/td3/fonctions.html' },
+      ]
+    },
+    {
+      name: 'TD4',
+      title: 'Programmation côté client',
+      icon: 'fas fa-puzzle-piece',
+      exercises: [
+        { name: 'Tri', link: './pages/td4/sorting.html' },
+        { name: 'Recherche', link: './pages/td4/searching.html' },
+      ]
+    },
+    {
+      name: 'TD5',
+      title: 'Programmation côté serveur',
+      icon: 'fas fa-project-diagram',
+      exercises: [
+        { name: 'Diagrammes de classes', link: './pages/td5/classes.html' },
+        { name: 'Diagrammes de séquence', link: './pages/td5/sequence.html' },
+      ]
+    },
+    {
+      name: 'TD7',
+      title: 'Échange de données',
+      icon: 'fas fa-envelope',
+      exercises: [
+        { name: 'Contact', link: './pages/td7/contact.html' },
+        { name: 'Validation', link: './pages/td7/signin.html' },
+      ]
+    },
+    {
+      name: 'TD8',
+      title: 'Ergonomie',
+      icon: 'fas fa-laptop-code',
+      exercises: [
+        { name: 'Exercice 1', link: './pages/td8/Exercice1.html' },
+        { name: 'Exercice 2', link: './pages/td8/Exercice2.html' },
+        { name: 'Exercice 3', link: './pages/td8/Exercice3.html' },
+        { name: 'Exercice 4', link: './pages/td8/Exercice4.html' },
+        { name: 'Exercice 5', link: './pages/td8/Exercice5.html' },
+        { name: 'La Firme', link: './pages/td8/la-firme.html' },
+      ]
+    }
+  ];
+  
